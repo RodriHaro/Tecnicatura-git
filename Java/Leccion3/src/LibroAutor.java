b7546f702fc48717cdfba8499a3f2e41038102ab
RodriHaro <rodri.nh@gmail.com>
2025-05-06 00:12:14 -0300

    Tecnicatura 5/5/2025

---
+++ Java/Leccion3/src/LibroAutor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LibroAutor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hola! como es tu nombre?: ");
+        var nombre = scanner.nextLine();
+
+        System.out.println("Hola " + nombre + ", ingresa el nombre del libro: ");
+        var nombreLibro = scanner.nextLine();
+
+        System.out.println("Ahora ingresa el nombre del autor: ");
+        var nombreAutor = scanner.nextLine();
+
+        System.out.println(nombreLibro + " fue escrito por " + nombreAutor);
+
+        scanner.close();
+    }
+}
