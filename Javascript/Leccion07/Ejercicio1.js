bf7fa2ba0cd29272e29ce7c84802e1a1ad1cafd4
RodriHaro <rodri.nh@gmail.com>
2025-05-19 20:38:09 -0300

    Ejercicios 1 y 2 de Clase 7: números pares e impares y es mayor de edad

---
+++ Javascript/Leccion07/Ejercicio1.js
@@ -0,0 +1,11 @@
+// Ejercicio 1: Encontrar numeros pares e impares
+
+let numero = parseInt(prompt("Ingrese un numero: "));
+
+if (numero % 2 === 0) {
+    console.log("El numero es par");
+} else {
+    console.log("El numero es impar");
+}
+
+
---
+++ Javascript/Leccion07/Ejercicio2.js
@@ -0,0 +1,10 @@
+// Ejercicio 2: Es mayor de edad
+
+let edad = parseInt(prompt("Ingrese su edad: "));
+
+if (edad >= 18) {
+    console.log("Es mayor de edad");
+} else {
+    console.log("Es menor de edad");
+}
+
