6c6983751401839a5aa0964fdd5fc69a99e92be3
RodriHaro <rodri.nh@gmail.com>
2025-06-25 20:51:26 -0300

    Leccion04 JavaScript: Ejercicio de meses del año agregado

--- Javascript/Leccion04/04-01-Ejercicios.js
+++ Javascript/Leccion04/04-01-Ejercicios.js
@@ -64,5 +64,120 @@ switch(mes){ //No solo se pueden utilizar número, también cadenas
 }
 console.log("Bienvenido a la estación de: "+estacion);
 
+//Ampliando el uso de var let y const
+/*
+Con var puedes reasignar en cualquier momento 
+este forma parte del ámbito global
+Un error es que se sobreescriba
+*/
+
+var nombre = 'Ariel';
+nombre = 'Osvaldo';
+console.log(nombre);
+
+function saludar(){
+    var nombre3 = 'Natalia';
+    console.log(nombre3);
+}
+//console.log(nombre3); //Aquí no lee el dato en la función
+
+if(true){
+    var edad = 34;
+    console.log(edad);
+}
+console.log(edad); //En la función funciono correctamente, en la estructura if fallo
+
+/*
+let: esta puede ser reasignada en cualquier momento
+la diferencia es que su ámbito es de bloque,
+solo disponible dentro de un bloque de llaves
+o dentro de una función
+*/
+
+function saludar2(){
+    let nombre2 = 'Ariel';
+    console.log(nombre2);
+}
+//console.log(nombre2);
+
+if(true){
+    let edad2 = 33;
+    console.log(edad2);
+}
+//console.log(edad2);
+
+/*
+const se utiliza para valores constantes que no pueden ser reasignadas
+*/
+
+const fechaNacimiento = 2006;
+console.log(fechaNacimiento);
+//fechaNacimiento = 2003;
+//console.log(fechaNacimiento); //solo se ejecuta el console anterior
+
+//Evitar repetir tu código
+//Dry don't repeat yourself
+let days = 5;
+
+switch (days) {
+    case 1:
+        console.log('Hoy es lunes');
+        break;
+    case 2:
+        console.log('Hoy es martes');
+        break;
+    case 3:
+        console.log('Hoy es miercoles');
+        break;
+    case 4:
+        console.log('Hoy es jueves');
+        break;
+    case 5:
+        console.log('Hoy es viernes');
+        break;
+    case 6:
+        console.log('Hoy es sabado');
+        break;
+    case 7:
+        console.log('Hoy es domingo');
+        break;
+    default:
+        console.log("Error en el ingreso del dia de la semana");
+        break;
+}
+
+//Esta es la opcion mejorada
+
+
+let days2 = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+function getDay(n){
+    if(n < 1 || n > 7){
+        throw new Error('out of range');
+    }
+    return days2[n-1];
+}
+
+console.log(getDay(3));
+
+//Hacer un ejercicio similar al que esta hecho, pero ahora con los
+//meses del año, debes hacerlo con la estructura switch y luego con
+//la función en la opción mejorada
+
+//Ejercicio con meses del año - Estructura switch
+
+let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 
+              'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+function getMes(n){
+    if(n < 1 || n > 12){
+        throw new Error('Mes fuera de rango (1-12)');
+    }
+    return meses[n-1];
+}
+
+console.log('El mes número 8 es: ' + getMes(8));
+console.log('El mes número 12 es: ' + getMes(12));
+console.log('El mes número 1 es: ' + getMes(1));
+
 
 
